0743eb17502e4280ee9df6d7864ac752313fb565
Bernardo <bernardoorosa@gmail.com>
2021-11-01 09:36:34 -0300

    Add env var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+const data = process.env.data || "World"
+app.get('/', (req, res) => res.send(`Hello ${data}!`))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
