e61a4321573265ce8a99f6a75c092275d919d0b8
Bernardo <bernardoorosa@gmail.com>
2021-11-29 06:49:48 -0300

    remove exclamations

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 const data = process.env.data || "World"
-app.get('/', (req, res) => res.send(`Hello ${data}!!!`))
+app.get('/', (req, res) => res.send(`Hello ${data}`))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
