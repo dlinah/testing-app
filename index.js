6e686855a38caa56883d5d38b675a24ae823bb03
Ezzat <A.ezzat1997@gmail.com>
2022-01-03 08:53:38 +0200

    test to run the new versio

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 const data = process.env.data || "World"
-app.get('/', (req, res) => res.send(`Hello ${data}!!!`))
+app.get('/', (req, res) => res.send(`Hello ${data} _1 !!!`))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
