4a7561ee243720377b1691b7dffe7ae171743333
Abd El Rahman Ezzat <a.ezzat1997@gmail.com>
2022-02-16 13:22:19 +0200

    adding some app logs

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const data = process.env.data || "World";
-app.get("/", (req, res) => res.send(`Hello ${data} _21!!!`));
+
+app.get("/", (req, res) => {
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    console.log("Visiting App at: ", time, process.env.data);
+    res.send(`Hello ${data} _21!!!`);
+
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
