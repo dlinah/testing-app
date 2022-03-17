a142dfdc718f7085da8d76882786d18c8ac624ed
Bernardo <bernardoorosa@gmail.com>
2022-03-17 17:05:12 -0300

    Test github installationFix

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const port = 3000;
 const data = process.env.data || "World";
 
 app.get("/", (req, res) => {
-    res.send(`Hello ${data}`);
+    res.send(`Hello ${data}!`);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
