573bfff4503e8b9e0e0e9b98b95e9bd51abc2a91
lina <linah.ahmed.92@gmail.com>
2022-11-15 23:29:51 +0200

    test

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const data = process.env.data || "Worlds";
+const data = process.env.data || "World!";
 
 app.get("/", (req, res) => {
-    console.log(`Hello ${data}!`)
+    console.log(`Hello ya ${data}!`)
     res.send(`Hello ${data}!`);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
