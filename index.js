908628288c45ca6e012ba4f41aa320af9f28224c
lina <linah.ahmed.92@gmail.com>
2023-03-18 14:27:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const data = process.env.data || "World!";
 
 app.get("/", (req, res) => {
-    console.log(`Hello 13 ${data}!`)
+    console.log(`Hello 14 ${data}!`)
     res.send(`Hello ${data}!`);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
