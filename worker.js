096a69cbbaa9d95a7ed543cd0b3033d248fabcde
lina <linah.ahmed.92@gmail.com>
2023-05-29 16:47:05 +0300

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,12 @@
+function worker() {
+  const now = new Date().getTime();
+ let s=0
+  while (s<1500) {
+    if(new Date().getTime() > now+s*1000){
+    console.log(s)
+      s+=1
+    }
+  }
+}
+
+worker();
